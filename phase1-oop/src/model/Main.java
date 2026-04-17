7212f7ac42507e503d484951d0967f1f64afc972
Anuj Srivastava <anujshrivastav128@gmail.com>
2026-04-17 23:58:30 +0530

    feat: implemented multithreading simulation and fixed race condition using synchronized method

--- phase1-oop/src/model/Main.java
+++ phase1-oop/src/model/Main.java
@@ -10,27 +10,32 @@ public class Main {
         Movie movie1 = new Movie(1, "Inception", "Sci Fi", 4.8);
         
         Show show1 = new Show(1, movie1, "10:00 AM");
-        Show show2 = new Show(2, movie1, "08:00 PM");
-
         service.addShow(show1);
-        service.addShow(show2);
 
         User user = new User(101, "Anuj");
 
-        Booking booking1 = service.bookTicket(user, 1, 5);
-        
-        System.out.println(booking1);
+        Runnable task1 = () -> {
+            try {
+                Booking b = service.bookTicket(user, 1, 7);
+                System.out.println("User1 booked: " + b);
+            } catch (Exception e) {
+                System.out.println("User1 failed: " + e.getMessage());
+            }
+        };
 
-        try{
-            Booking booking2 = service.bookTicket(user, 1, 5);
-            System.out.println(booking2); 
-        } catch(Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        } 
+        Runnable task2 = () -> {
+            try {
+                Booking b = service.bookTicket(user, 1, 7);
+                System.out.println("User2 booked: " + b);
+            } catch (Exception e) {
+                System.out.println("User2 failed: " + e.getMessage());
+            }
+        };
 
-        System.out.println("Available Seats: " + show1.getAvailableSeats(10));
+        Thread t1 = new Thread(task1);
+        Thread t2 = new Thread(task2);
 
-        Booking booking3 = service.bookTicket(user, 2, 5);
-        System.out.println("Different show bookings: " + booking3);
+        t1.start();
+        t2.start();
     }
 }
