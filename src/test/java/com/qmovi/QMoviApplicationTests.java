0b5a98c62ef4b959d40925c42d2cbcb9f2bcc84e
Anuj Srivastava <anujshrivastav128@gmail.com>
2026-05-10 02:13:50 +0530

    feat: bootstrap Spring Boot architecture with Gradle and PostgreSQL

---
+++ src/test/java/com/qmovi/QMoviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qmovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QMoviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
